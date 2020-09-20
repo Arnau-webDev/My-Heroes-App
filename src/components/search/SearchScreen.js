1ecaa46a0fe9a20409de729ccc0133b91accc8a3
ArnauMQ <arnaumajob@gmail.com>
2020-09-20 08:53:07 +0200

    Initial App Funcionality

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,54 @@
+import React, { useMemo, useState } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+import { HeroCard } from '../heroes/HeroCard';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { useForm } from '../../hooks/useForm';
+
+export const SearchScreen = ({ history }) => {
+	const location = useLocation();
+	const { q = '' } = queryString.parse(location.search);
+
+	const [formValues, handleInputChange, reset] = useForm({
+		searchText: q,
+	});
+	const { searchText } = formValues;
+
+	const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		history.push(`?q=${searchText}`);
+		reset();
+	};
+
+	return (
+		<div className="animate__animated animate__fadeIn">
+			<div className="row">
+				<div className="col">
+					<h4>Search Form</h4>
+					<form className="form-group" onSubmit={handleSearch}>
+						<input
+							type="text"
+							autoComplete="off"
+							placeholder="Find your hero"
+							name="searchText"
+							className="form-control"
+							onChange={handleInputChange}
+							value={searchText}
+						/>
+
+						<button className="btn mt-3 btn-block btn-outline-primary">
+							Search...
+						</button>
+					</form>
+				</div>
+			</div>
+			<div className="row p-3">
+				{heroesFiltered.map((hero) => {
+					return <HeroCard hero={hero} key={hero.id} className="col-6" />;
+				})}
+			</div>
+		</div>
+	);
+};
