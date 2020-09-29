27f31df9bd215e475b981c64dafc35085b2acf8a
ArnauMQ <arnauma16@gmail.com>
2020-09-29 20:42:22 +0200

    Added route protection

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/AuthContext';
 
 export const LoginScreen = ({ history }) => {
+	const lastPathVisited = localStorage.getItem("lastPath");
+	const { dispatch } = useContext(AuthContext);
+
 	const handleLogin = () => {
-		// history.push('/marvel');
-		history.replace('/');
+		dispatch({
+			type: types.login,
+			payload: {name: "Arnau", logged: false}
+		});
+		
+		if(!!lastPathVisited) {
+			history.replace(lastPathVisited);
+		} else {
+			history.replace('/');
+		}
 	};
 
 	return (
